99b7bc3dc65820a75275df2afb9bdd595ee60973
cyber-being1 <th.veekeshsingh123@gmail.com>
2026-09-17 00:48:35 +0530

    Update HMS product API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -6,4 +7,27 @@ app = FastAPI()
 def greet():
     return "Welcome to HMS"
 
+products = [
+    Product(id= 1,name="phone",description="budget phone",price=99,quantity=10),
+    Product(id= 2,name="laptop",description="gaming laptop",price=999,quantity=6),
+    Product(id= 3,name="tablet",description="hd tablet",price=199,quantity=5),
+    Product(id= 4,name="watch",description="smart watch",price=199,quantity=8),
+   
+]
 
+@app.get("/products")
+def get_all_products():
+    return products
+
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+    return {"error": "Product not found"}
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
